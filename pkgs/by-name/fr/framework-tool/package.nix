4a865ab6e3a547711d8cbeaae222d6fbe59588df
Adam Dinwoodie <adam@dinwoodie.org>
2024-12-16 21:44:48 +0000

    framework-tool: 0.1.0-unstable-2024-06-14 -> 0.1.0-unstable-2024-12-11

--- pkgs/by-name/fr/framework-tool/package.nix
+++ pkgs/by-name/fr/framework-tool/package.nix
@@ -5,13 +5,13 @@ rustPlatform.buildRustPackage {
 
   # Latest stable version 0.1.0 has an ssh:// git URL in Cargo.lock,
   # so use unstable for now
-  version = "0.1.0-unstable-2024-06-14";
+  version = "0.1.0-unstable-2024-12-11";
 
   src = fetchFromGitHub {
     owner = "FrameworkComputer";
     repo = "framework-system";
-    rev = "705805ce3fd9acf23ae4e310227ca62b7d686a69";
-    hash = "sha256-crMA0jdCGNDvwTzYXiDpz+1O2Tk84j5cLcQAaplCDFs=";
+    rev = "fb432289d4accfa88997f3f2e118fad4838e9865";
+    hash = "sha256-Iz/Mcwj2QXD67/ry69RRfFgbNECAURJzpE6P+Mjd//I=";
   };
 
   cargoLock = {
@@ -20,6 +20,7 @@ rustPlatform.buildRustPackage {
       "smbios-lib-0.9.1" =
         "sha256-3L8JaA75j9Aaqg1z9lVs61m6CvXDeQprEFRq+UDCHQo=";
       "uefi-0.20.0" = "sha256-/3WNHuc27N89M7s+WT64SHyFOp7YRyzz6B+neh1vejY=";
+      "redox_hwio-0.1.6" = "sha256-knLIZ7yp42SQYk32NGq3SUGvJFVumFhD64Njr5TRdFs=";
     };
   };
 
