1d8402677dbc80a80c023d3a6cb154774adb75ac
Adam Dinwoodie <adam@dinwoodie.org>
2026-03-31 11:34:50 +0100

    make*wrapper: refactor
    - Use ${var@Q} for quoting/escaping arguments in error messages
    - Remove `set -euo pipefail` from make-binary-wrapper.sh for consistency
      with make-wrapper.sh
    - Provide useful error messages when using arguments only supported by
      makeShellWrapper with makeBinaryWrapper
    
    - TODO: finish this job: argument lists should be the same.
    
    - use subshell in makeShellWrapper to avoid needing to reset environment
    - use printf rather than echo as best practice for printing variables
      provided by the user, and for the neater handling of escaping (e.g.
      if separator is `:`, then `printf '%q' "$separator"` produces `:`,
      where `echo "${separator@Q}"` produces `':'`)
    - Remove unnecessary braces around for loop in makeShellWrapper's
      addValue
    - separate flags from arguments using `--` where that's supported and
      the arguments aren't explicitly given without an initial `-`
    - add TODO for wrangling stdout
    - remove unused addSuffix and filterExisting functions
    - use bash string manipulation to avoid dirname/basename calls
    - consistent shell whitespace: 2sp indents per .editorconfig, rather
      than a mix of 2sp and 4sp

--- pkgs/build-support/setup-hooks/make-wrapper2.sh
+++ pkgs/build-support/setup-hooks/make-wrapper2.sh
@@ -2,9 +2,9 @@
 #
 # assertExecutable FILE
 assertExecutable() {
-    local file="$1"
-    [[ -f "$file" && -x "$file" ]] || \
-        die "Cannot wrap '$file' because it is not an executable file"
+  local file="$1"
+  [[ -f "$file" && -x "$file" ]] || \
+    die "Cannot wrap ${file@Q} because it is not an executable file"
 }
 
 # construct an executable file that wraps the actual executable
@@ -38,198 +38,190 @@ assertExecutable() {
 # --suffix-contents
 makeWrapper() { makeShellWrapper "$@"; }
 makeShellWrapper() {
-    local original="$1"
-    local wrapper="$2"
-    local params varName value command separator n fileNames
-    local argv0 flagsBefore flagsAfter flags
+  local original="$1"
+  local wrapper="$2"
+  local params varName value command separator n fileNames
+  local argv0 flagsBefore flagsAfter flags
 
-    assertExecutable "$original"
+  assertExecutable "$original"
 
-    # Write wrapper code which adds `value` to the beginning or end of
-    # the list variable named by `varName`, depending on the `mode`
-    # specified.
-    #
-    # A value which is already part of the list will not be added
-    # again. If this is the case and the `suffix` mode is used, the
-    # list won't be touched at all. The `prefix` mode will however
-    # move the last matching instance of the value to the beginning
-    # of the list. Any remaining duplicates of the value will be left
-    # as-is.
-    addValue() {
-        local mode="$1"       # `prefix` or `suffix` to add to the beginning or end respectively
-        local varName="$2"    # name of list variable to add to
-        local separator="$3"  # character used to separate elements of list
-        local value="$4"      # one value, or multiple values separated by `separator`, to add to list
+  # Write wrapper code which adds `value` to the beginning or end of
+  # the list variable named by `varName`, depending on the `mode`
+  # specified.
+  #
+  # A value which is already part of the list will not be added
+  # again. If this is the case and the `suffix` mode is used, the
+  # list won't be touched at all. The `prefix` mode will however
+  # move the last matching instance of the value to the beginning
+  # of the list. Any remaining duplicates of the value will be left
+  # as-is.
+  addValue() (
+    local mode="$1"       # `prefix` or `suffix` to add to the beginning or end respectively
+    local varName="$2"    # name of list variable to add to
+    local separator="$3"  # character used to separate elements of list
+    local value="$4"      # one value, or multiple values separated by `separator`, to add to list
 
-        # Disable file globbing, since bash will otherwise try to find
-        # filenames matching the the value to be prefixed/suffixed if
-        # it contains characters considered wildcards, such as `?` and
-        # `*`. We want the value as is, except we also want to split
-        # it on on the separator; hence we can't quote it.
-        local reenableGlob=0
-        if [[ ! -o noglob ]]; then
-            reenableGlob=1
-        fi
-        set -o noglob
+    # Disable file globbing, since bash will otherwise try to find
+    # filenames matching the the value to be prefixed/suffixed if
+    # it contains characters considered wildcards, such as `?` and
+    # `*`. We want the value as is, except we also want to split
+    # it on on the separator; hence we can't quote it.
+    set -o noglob
 
-        if [[ -n "$value" ]]; then
-            local old_ifs=$IFS
-            IFS=$separator
+    if [[ -n "$value" ]]; then
+      IFS=$separator
 
-            if [[ "$mode" == '--prefix'* ]]; then
-                # Keep the order of the components as written when
-                # prefixing; normally, they would be added in the
-                # reverse order.
-                local tmp=
-                for v in $value; do
-                    tmp=$v${tmp:+$separator}$tmp
-                done
-                value="$tmp"
-            fi
-            for v in $value; do
-                {
-                    echo "$varName=\${$varName:+${separator@Q}\$$varName${separator@Q}}"               # add separators on both ends unless empty
-                    if [[ "$mode" == '--prefix'* ]]; then                                              # -- in prefix mode --
-                        echo "$varName=\${$varName/${separator@Q}${v@Q}${separator@Q}/${separator@Q}}" # remove the first instance of the value (if any)
-                        echo "$varName=${v@Q}\$$varName"                                               # prepend the value
-                    elif [[ "$mode" == '--suffix'* ]]; then                                            # -- in suffix mode --
-                        echo "if [[ \$$varName != *${separator@Q}${v@Q}${separator@Q}* ]]; then"       # if the value isn't already in the list
-                        echo "    $varName=\$$varName${v@Q}"                                           # append the value
-                        echo "fi"
-                    else
-                        echo "unknown mode $mode!" 1>&2
-                        exit 1
-                    fi
-                    echo "$varName=\${$varName#${separator@Q}}"                                        # remove leading separator
-                    echo "$varName=\${$varName%${separator@Q}}"                                        # remove trailing separator
-                    echo "export $varName"
-                } >> "$wrapper"
-            done
-            IFS=$old_ifs
-        fi
-
-        if (( reenableGlob )); then
-            set +o noglob
-        fi
-    }
-
-    mkdir -p "$(dirname "$wrapper")"
-
-    echo "#! @shell@ -e" > "$wrapper"
-
-    params=("$@")
-    for ((n = 2; n < ${#params[*]}; n += 1)); do
-        p="${params[$n]}"
-
-        if [[ "$p" == "--set" ]]; then
-            varName="${params[$((n + 1))]}"
-            value="${params[$((n + 2))]}"
-            n=$((n + 2))
-            echo "export $varName=${value@Q}" >> "$wrapper"
-        elif [[ "$p" == "--set-default" ]]; then
-            varName="${params[$((n + 1))]}"
-            value="${params[$((n + 2))]}"
-            n=$((n + 2))
-            echo "export $varName=\${$varName-${value@Q}}" >> "$wrapper"
-        elif [[ "$p" == "--unset" ]]; then
-            varName="${params[$((n + 1))]}"
-            n=$((n + 1))
-            echo "unset $varName" >> "$wrapper"
-        elif [[ "$p" == "--chdir" ]]; then
-            dir="${params[$((n + 1))]}"
-            n=$((n + 1))
-            echo "cd ${dir@Q}" >> "$wrapper"
-        elif [[ "$p" == "--run" ]]; then
-            command="${params[$((n + 1))]}"
-            n=$((n + 1))
-            echo "$command" >> "$wrapper"
-        elif [[ ("$p" == "--suffix") || ("$p" == "--prefix") ]]; then
-            varName="${params[$((n + 1))]}"
-            separator="${params[$((n + 2))]}"
-            value="${params[$((n + 3))]}"
-            n=$((n + 3))
-            addValue "$p" "$varName" "$separator" "$value"
-        elif [[ ("$p" == "--suffix-each") || ("$p" == "--prefix-each") ]]; then
-            varName="${params[$((n + 1))]}"
-            separator="${params[$((n + 2))]}"
-            values="${params[$((n + 3))]}"
-            n=$((n + 3))
-            for value in $values; do
-                addValue "$p" "$varName" "$separator" "$value"
-            done
-        elif [[ ("$p" == "--suffix-contents") || ("$p" == "--prefix-contents") ]]; then
-            varName="${params[$((n + 1))]}"
-            separator="${params[$((n + 2))]}"
-            fileNames="${params[$((n + 3))]}"
-            n=$((n + 3))
-            for fileName in $fileNames; do
-                contents="$(cat "$fileName")"
-                addValue "$p" "$varName" "$separator" "$contents"
-            done
-        elif [[ "$p" == "--add-flag" ]]; then
-            flags=${params[n + 1]@Q}
-            n=$((n + 1))
-            flagsBefore="${flagsBefore-} $flags"
-        elif [[ "$p" == "--append-flag" ]]; then
-            flags=${params[n + 1]@Q}
-            n=$((n + 1))
-            flagsAfter="${flagsAfter-} $flags"
-        elif [[ "$p" == "--add-flags" ]]; then
-            flags="${params[$((n + 1))]}"
-            n=$((n + 1))
-            flagsBefore="${flagsBefore-} $flags"
-        elif [[ "$p" == "--append-flags" ]]; then
-            flags="${params[$((n + 1))]}"
-            n=$((n + 1))
-            flagsAfter="${flagsAfter-} $flags"
-        elif [[ "$p" == "--argv0" ]]; then
-            argv0="${params[$((n + 1))]}"
-            n=$((n + 1))
-        elif [[ "$p" == "--inherit-argv0" ]]; then
-            # Whichever comes last of --argv0 and --inherit-argv0 wins
-            argv0='$0'
-        elif [[ "$p" == "--resolve-argv0" ]]; then
-            # this is noop in shell wrappers, since bash will always resolve $0
-            resolve_argv0=1
+      if [[ "$mode" == '--prefix'* ]]; then
+        # Keep the order of the components as written when
+        # prefixing; normally, they would be added in the
+        # reverse order.
+        local tmp=
+        for v in $value; do
+          tmp=$v${tmp:+$separator}$tmp
+        done
+        value="$tmp"
+      fi
+      for v in $value; do
+        # Add separators on both ends unless the variable is empty.
+        printf '%s="${%s:+%q"$%s"%q}"\n' "$varName" "$varName" "$separator" "$varName" "$separator"
+        if [[ "$mode" == '--prefix'* ]]; then
+          # In prefix mode, remove the first instance of the value
+          # (if any) from the variable, then prepend the value.
+          printf '%s="${%s/%q%q%q/%q}"\n' "$varName" "$varName" "$separator" "$v" "$separator" "$separator"
+          printf '%s=%q"$%s"\n' "$varName" "$v" "$varName"
+        elif [[ "$mode" == '--suffix'* ]]; then
+          # In suffix mode, add the value to the list if and only if
+          # it isn't already in the list.
+          printf 'if [[ "$%s" != *%q%q%q* ]]; then\n' "$varName" "$separator" "$v" "$separator"
+          printf '    %s="$%s"%q\n' "$varName" "$varName" "$v"
+          printf 'fi\n'
         else
-            die "makeWrapper doesn't understand the arg $p"
+          echo "unknown mode $mode!" 1>&2
+          exit 1
         fi
-    done
+        # Remove any separators at the start and end, and export the variable.
+        printf '%s="${%s#%q}"\n' "$varName" "$varName" "$separator"
+        printf '%s="${%s%%%q}"\n' "$varName" "$varName" "$separator"
+        printf 'export %s\n' "$varName"
+      done >>"$wrapper"
+    fi
+  )
 
-    echo exec ${argv0:+-a \"$argv0\"} \""$original"\" \
-         "${flagsBefore-}" '"$@"' "${flagsAfter-}" >> "$wrapper"
+  if [[ "$wrapper" = */* ]]; then
+    mkdir -p -- "${wrapper%/*}"
+  fi
 
-    chmod +x "$wrapper"
-}
+  # TODO Drop the >"$wrapper" nonsense by changing stdout to the wrapper then
+  # changing it back when finished.
+  echo "#! @shell@ -e" > "$wrapper"
 
-addSuffix() {
-    suffix="$1"
-    shift
-    for name in "$@"; do
-        echo "$name$suffix"
-    done
-}
+  params=("$@")
+  for ((n = 2; n < ${#params[*]}; n += 1)); do
+    p="${params[$n]}"
 
-filterExisting() {
-    for fn in "$@"; do
-        if test -e "$fn"; then
-            echo "$fn"
-        fi
-    done
+    if [[ "$p" == "--set" ]]; then
+      varName="${params[$((n + 1))]}"
+      value="${params[$((n + 2))]}"
+      n=$((n + 2))
+      printf 'export %s=%q\n' "$varName" "$value" >>"$wrapper"
+    elif [[ "$p" == "--set-default" ]]; then
+      varName="${params[$((n + 1))]}"
+      value="${params[$((n + 2))]}"
+      n=$((n + 2))
+      printf 'export %s="${%s-%q}"\n' "$varName" "$varName" "$value" >>"$wrapper"
+    elif [[ "$p" == "--unset" ]]; then
+      varName="${params[$((n + 1))]}"
+      n=$((n + 1))
+      printf 'unset %s\n' "$varName" >>"$wrapper"
+    elif [[ "$p" == "--chdir" ]]; then
+      dir="${params[$((n + 1))]}"
+      n=$((n + 1))
+      printf 'cd -- %q\n' "$dir" >>"$wrapper"
+    elif [[ "$p" == "--run" ]]; then
+      command="${params[$((n + 1))]}"
+      n=$((n + 1))
+      printf '%s\n' "$command" >>"$wrapper"
+    elif [[ ("$p" == "--suffix") || ("$p" == "--prefix") ]]; then
+      varName="${params[$((n + 1))]}"
+      separator="${params[$((n + 2))]}"
+      value="${params[$((n + 3))]}"
+      n=$((n + 3))
+      addValue "$p" "$varName" "$separator" "$value"
+    elif [[ ("$p" == "--suffix-each") || ("$p" == "--prefix-each") ]]; then
+      varName="${params[$((n + 1))]}"
+      separator="${params[$((n + 2))]}"
+      values="${params[$((n + 3))]}"
+      n=$((n + 3))
+      for value in $values; do
+        addValue "$p" "$varName" "$separator" "$value"
+      done
+    elif [[ ("$p" == "--suffix-contents") || ("$p" == "--prefix-contents") ]]; then
+      varName="${params[$((n + 1))]}"
+      separator="${params[$((n + 2))]}"
+      fileNames="${params[$((n + 3))]}"
+      n=$((n + 3))
+      for fileName in $fileNames; do
+        contents="$(cat "$fileName")"
+        addValue "$p" "$varName" "$separator" "$contents"
+      done
+    elif [[ "$p" == "--add-flag" ]]; then
+      flags=${params[n + 1]@Q}
+      n=$((n + 1))
+      flagsBefore="${flagsBefore-} $flags"
+    elif [[ "$p" == "--append-flag" ]]; then
+      flags=${params[n + 1]@Q}
+      n=$((n + 1))
+      flagsAfter="${flagsAfter-} $flags"
+    elif [[ "$p" == "--add-flags" ]]; then
+      flags="${params[$((n + 1))]}"
+      n=$((n + 1))
+      flagsBefore="${flagsBefore-} $flags"
+    elif [[ "$p" == "--append-flags" ]]; then
+      flags="${params[$((n + 1))]}"
+      n=$((n + 1))
+      flagsAfter="${flagsAfter-} $flags"
+    elif [[ "$p" == "--argv0" ]]; then
+      argv0="${params[$((n + 1))]}"
+      n=$((n + 1))
+    elif [[ "$p" == "--inherit-argv0" ]]; then
+      # Whichever comes last of --argv0 and --inherit-argv0 wins
+      argv0='$0'
+    elif [[ "$p" == "--resolve-argv0" ]]; then
+      # this is noop in shell wrappers, since bash will always resolve $0
+      resolve_argv0=1
+    else
+      die "makeWrapper doesn't understand the arg $p"
+    fi
+  done
+
+  printf 'exec ' >>"$wrapper"
+  [[ -v argv0 && "$argv0" ]] && printf -- '-a %q ' "$argv0" >>"$wrapper"
+  printf -- '-- %q ' "$original" >>"$wrapper"
+  [[ -v flagsBefore ]] && printf '%s ' "$flagsBefore" >>"$wrapper"
+  printf '"$@"' >>"$wrapper"
+  [[ -v flagsAfter ]] && printf ' %s' "$flagsAfter" >>"$wrapper"
+  printf '\n' >>"$wrapper"
+
+  chmod +x -- "$wrapper"
 }
 
 # Syntax: wrapProgram <PROGRAM> <MAKE-WRAPPER FLAGS...>
 wrapProgram() { wrapProgramShell "$@"; }
 wrapProgramShell() {
-    local prog="$1"
-    local hidden
+  local prog="$1"
+  local hidden
 
-    assertExecutable "$prog"
+  assertExecutable "$prog"
 
-    hidden="$(dirname "$prog")/.$(basename "$prog")"-wrapped
-    while [ -e "$hidden" ]; do
-      hidden="${hidden}_"
-    done
-    mv "$prog" "$hidden"
-    makeShellWrapper "$hidden" "$prog" --inherit-argv0 "${@:2}"
+  if [[ "$prog" = */* ]]; then
+    hidden="${prog%/*}/.${prog##*/}-wrapped"
+  else
+    hidden=".${prog}-wrapped"
+  fi
+
+  while [ -e "$hidden" ]; do
+    hidden="${hidden}_"
+  done
+  mv -- "$prog" "$hidden"
+  makeShellWrapper "$hidden" "$prog" --inherit-argv0 "${@:2}"
 }
